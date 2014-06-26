e795114a216f6948bc44f973b45e96eba938c6b6
Longgeek <longgeek@thstack.com>
2014-06-26 20:19:15 +0800

    Update puppet coding style
    Change-Id: I99eb0b2ed2f78be179605595105bfc798897cd3e
    Closes-Bug: #1334627

--- hot/software-config/example-templates/config-scripts/example-puppet-manifest.pp
+++ hot/software-config/example-templates/config-scripts/example-puppet-manifest.pp
@@ -1,12 +1,13 @@
-file {'barfile':
-  ensure  => file,
-  mode    => 0644,
-  path    => "/tmp/$::bar",
-  content => "$::foo",
+file { 'barfile':
+    ensure  => file,
+    mode    => '0644',
+    path    => '/tmp/$::bar',
+    content => '$::foo',
 }
-file {'output_result':
-  ensure  => file,
-  path    => "$::heat_outputs_path.result",
-  mode    => 0644,
-  content => "The file /tmp/$::bar contains $::foo",
+
+file { 'output_result':
+    ensure  => file,
+    path    => '$::heat_outputs_path.result',
+    mode    => '0644',
+    content => 'The file /tmp/$::bar contains $::foo',
 }
