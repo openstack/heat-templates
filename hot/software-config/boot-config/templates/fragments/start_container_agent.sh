345871e6846dac75c6ee8ce2654ad4d1fc95d1dd
Rabi Mishra <ramishra@redhat.com>
2015-06-03 22:21:01 +0530

    Fix folder name and update docker-compose version
    Fixes folder name for script. Pins fedora base image to f21,
    as latest fedora base image(f22) has number of changes that
    breaks docker build. Upgrades docker-compose to latest
    version that supports `--pid=host` (required by tripleo).
    
    Change-Id: Ia4e32f1461bfc9278e25c9236dc911f1b9c4e876

--- hot/software-config/boot-config/templates/fragments/start_container_agent.sh
+++ hot/software-config/boot-config/templates/fragments/start_container_agent.sh
@@ -14,7 +14,7 @@ User=root
 Restart=on-failure
 ExecStartPre=-/usr/bin/docker kill heat-container-agent
 ExecStartPre=-/usr/bin/docker rm heat-container-agent
-ExecStartPre=/opt/agent_container/get_container_agent_image.sh $agent_image
+ExecStartPre=/opt/container_agent/get_container_agent_image.sh $agent_image
 ExecStart=/usr/bin/docker run --name heat-container-agent --privileged --net=host -v /usr/bin/atomic:/usr/bin/atomic -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/atomic:/usr/bin/atomic -v /var/lib/cloud:/var/lib/cloud -v /var/lib/heat-cfntools:/var/lib/heat-cfntools $agent_image
 ExecStop=/usr/bin/docker stop heat-container-agent
 
@@ -29,6 +29,6 @@ EOF
 
 # enable and start heat-container-agent
 chmod 0640 /etc/systemd/system/heat-container-agent.service
-chmod 0755 /opt/agent_container/get_agent_container_image.sh
+chmod 0755 /opt/container_agent/get_container_agent_image.sh
 /usr/bin/systemctl enable heat-container-agent.service
 /usr/bin/systemctl start --no-block heat-container-agent.service
