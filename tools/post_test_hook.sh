1ea408fc3193da546099b3b6595d21c9e5bdf74e
Thomas Herve <therve@redhat.com>
2017-05-18 10:06:21 +0200

    Fake endpoint creation
    Instead of instantiating the services, fake their creation in the
    catalog to allow Heat to do validation.
    
    Change-Id: Ic6370fa18a3fae369c22ffe0930daa7c7c42c494

--- tools/post_test_hook.sh
+++ tools/post_test_hook.sh
@@ -16,5 +16,24 @@
 
 set -x
 
-source $BASE/new/devstack/openrc
+source $BASE/new/devstack/openrc admin admin
+source $BASE/new/devstack/functions-common
+neutron_service=$(get_or_create_service "neutron" "network" "Neutron Service")
+get_or_create_endpoint $neutron_service "$REGION_NAME" "http://localhost"
+aodh_service=$(get_or_create_service "aodh" "alarming" "OpenStack Alarming Service")
+get_or_create_endpoint $aodh_service "$REGION_NAME" "http://localhost"
+mistral_service=$(get_or_create_service "mistral" "workflowv2" "Workflow Service v2")
+get_or_create_endpoint $mistral_service "$REGION_NAME" "http://localhost"
+senlin_service=$(get_or_create_service "senlin" "clustering" "Senlin Clustering Service")
+get_or_create_endpoint $senlin_service "$REGION_NAME" "http://localhost"
+monasca_service=$(get_or_create_service "monasca" "monitoring" "Monasca Monitoring Service")
+get_or_create_endpoint $monasca_service "$REGION_NAME" "http://localhost"
+zaqar_service=$(get_or_create_service "zaqar" "messaging" "Zaqar Service")
+get_or_create_endpoint $zaqar_service "$REGION_NAME" "http://localhost"
+designate_service=$(get_or_create_service "designate" "dns" "Designate DNS Service")
+get_or_create_endpoint $designate_service "$REGION_NAME" "http://localhost"
+barbican_service=$(get_or_create_service "barbican" "key-manager" "Barbican Service")
+get_or_create_endpoint $barbican_service "$REGION_NAME" "http://localhost"
+
+source $BASE/new/devstack/openrc demo demo
 python $BASE/new/heat-templates/tools/validate-templates $BASE/new/heat-templates
